305a464815c28f40d4ca039c873ec1c7cc1478e6
tbennett@unicosolution.com <tbennett@unicosolution.com>
2018-05-24 17:26:47 -0400

    updated deploy.sh for change in path and project name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 mvn validate
 mvn test
 mvn clean compile assembly:single
-mv ./target/crypto-1.0-jar-with-dependencies.jar ./target/crypto-1.0.jar
-mvn install:install-file -Dfile=./target/crypto-1.0.jar -DgroupId=com.metaopsis -DartifactId=crypto -Dversion=1.0 -Dpackaging=jar
+mv ./target/cryptor-1.0-jar-with-dependencies.jar ./target/cryptor-1.0.jar
+mvn install:install-file -Dfile=./target/cryptor-1.0.jar -DgroupId=security -DartifactId=cryptor -Dversion=1.0 -Dpackaging=jar
